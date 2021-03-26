7b3ff88766a9c0ba4c136ed1052149911e1c6f1f
Semyon Sorokin <semsor.ru@gmail.com>
2021-03-26 13:42:31 +0700

    add Snippets

---
+++ local/templates/blank/snippets/.content.php
@@ -0,0 +1,27 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<?
+$SNIPPETS = Array();
+$SNIPPETS['snippet0001.snp'] = Array('title' => 'Заголовок h1 со вторичным тестом');
+$SNIPPETS['snippet0002.snp'] = Array('title' => 'Заголовок h2 со вторичным тестом');
+$SNIPPETS['snippet0003.snp'] = Array('title' => 'Заголовок h3 со вторичным тестом');
+$SNIPPETS['snippet0004.snp'] = Array('title' => 'Заголовок h4 со вторичным тестом');
+$SNIPPETS['snippet0005.snp'] = Array('title' => 'Заголовок h5 со вторичным тестом');
+$SNIPPETS['snippet0006.snp'] = Array('title' => 'Заголовок h6 со вторичным тестом');
+$SNIPPETS['snippet0007.snp'] = Array('title' => 'Ведущий основной текст');
+$SNIPPETS['snippet0008.snp'] = Array('title' => 'Мелкий текст');
+$SNIPPETS['snippet0009.snp'] = Array('title' => 'Жирный шрифт');
+$SNIPPETS['snippet0010.snp'] = Array('title' => 'Курсивный текст', 'description' => 'Свободно используйте <b> и <i> в HTML5. Тег <b> предназначен для выделения слова или фразы без передачи дополнительного значения, в то время как <i> в основном для цитирования, технических терминов и т.д.');
+$SNIPPETS['snippet0011.snp'] = Array('title' => 'Выравнивание: слева');
+$SNIPPETS['snippet0012.snp'] = Array('title' => 'Выравнивание: по центру');
+$SNIPPETS['snippet0013.snp'] = Array('title' => 'Выравнивание: правому краю');
+$SNIPPETS['snippet0014.snp'] = Array('title' => 'Выравнивание: по ширине');
+$SNIPPETS['snippet0015.snp'] = Array('title' => 'Аббревиатура', 'description' => 'Внедрена стилизацию HTML-элементов <abbr> для аббревиатур и акронимов, чтобы показать расширенную их версию при наведении. Аббревиатуры с атрибутом title имеют внизу легкое точечное подчеркивания, а курсор принимает вид знака вопроса при наведении, предоставляя дополнительный контекст для зависания (hover).');
+$SNIPPETS['snippet0016.snp'] = Array('title' => 'Адрес');
+$SNIPPETS['snippet0017.snp'] = Array('title' => 'Цитата, текст слева');
+$SNIPPETS['snippet0018.snp'] = Array('title' => 'Цитата, с пометкой');
+$SNIPPETS['snippet0019.snp'] = Array('title' => 'Цитата, текст справа');
+$SNIPPETS['snippet0020.snp'] = Array('title' => 'Список без стилизации');
+$SNIPPETS['snippet0021.snp'] = Array('title' => 'Список встроенный (инлайновый)');
+$SNIPPETS['snippet0022.snp'] = Array('title' => 'Описание (dl dt dd)');
+$SNIPPETS['snippet0023.snp'] = Array('title' => 'Описание горизонтальное (dl dt dd)');
+?>
